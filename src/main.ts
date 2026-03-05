4dc5e720ac019b1a5ac670bc13ce13880fc163f4
MH_vietdh <viet.dh@mor.com.vn>
2026-03-05 11:28:16 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4173',
+    origin: true, // Cho phép mọi origin
     credentials: true,
   });
 
