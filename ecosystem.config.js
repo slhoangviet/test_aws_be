ff82a9c3697e0dda9ac94b8913580a6a4071277a
Hoàng Việt <sl.hoangviet@gmail.com>
2026-03-08 21:00:45 +0700

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,20 +3,12 @@ module.exports = {
     {
       name: 'test_aws_be',
       script: 'dist/main.js',
-      // Trước khi chạy PM2 production, build trước:
-      //   cd test_aws_be && npm install && npm run build
       instances: 1,
       exec_mode: 'fork',
       env: {
-        NODE_ENV: 'development',
-        PORT: 3000,
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 3000,
+        PORT: 3001,
       },
       watch: false,
     },
   ],
 };
-
